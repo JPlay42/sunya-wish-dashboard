c53b4723018d6a7b483c8db06acb0c00d865f745
JPlay <kuruch2004@gmail.com>
2020-07-30 15:42:20 +0300

    Залил веб-страницу

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const user = {
+  name: "Sunya",
+  surname: "2",
+  userPhoto: "photo.png",
+  qrQode: "qr-code.png",
+  dreams: [
+    { src: "tesla.jpg", alt: "Авто моєї мрії" },
+    { src: "karina.jpg", alt: "Зустріч з улюбленим блогером" },
+    { src: "paris.jpg", alt: "Подорож до Парижу" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  500,
+  800,
+  990,
+  1000,
+  1200,
+  1500,
+  1800,
+  2200,
+  2400,
+  2700,
+  3000,
+  3300,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Креативність" },
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Особистий бренд" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
+];
+
+export { user, arr, necessaryExpenses, todo };
